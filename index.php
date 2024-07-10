e0ab46e4f82ef4ef23caabac99dc9e2db9796d79
x3cb <allmyscamas@proton.me>
2024-07-10 22:54:55 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TeleGru </title>
+    <meta name="keywords" content="TeleGru | telegram, carding, cc, credit card, credit cards, fullz, dump, cloned, drug, drugs, ketamine, cocaine, cannabis, weed, mdma, ecstasy, xtc, document, documents, docs, licence, invoice, identity card, identity cards, id card, id cards">
+    <meta name="description" content="TeleGru | telegram, carding, cc, credit card, credit cards, fullz, dump, cloned, drug, drugs, ketamine, cocaine, cannabis, weed, mdma, ecstasy, xtc, document, documents, docs, licence, invoice, identity card, identity cards, id card, id cards">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="img/logo.png" type="image/x-icon">
+</head>

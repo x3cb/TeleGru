47b49477a015b97d7c3d6e3a9f5ce157c8b9ea9c
x3cb <allmyscamas@proton.me>
2024-11-15 13:59:50 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TeleGru </title>
-    <meta name="keywords" content="TeleGru | telegram, carding, cc, credit card, credit cards, fullz, dump, cloned, drug, drugs, ketamine, cocaine, cannabis, weed, mdma, ecstasy, xtc, document, documents, docs, licence, invoice, identity card, identity cards, id card, id cards">
-    <meta name="description" content="TeleGru | telegram, carding, cc, credit card, credit cards, fullz, dump, cloned, drug, drugs, ketamine, cocaine, cannabis, weed, mdma, ecstasy, xtc, document, documents, docs, licence, invoice, identity card, identity cards, id card, id cards">
+    <meta name="description" content="Best Multilingual Telegram bots for Buying on the Darknet.">
+    <meta name="keywords" content="TeleGru, telegram, hack, carding, cc, credit card, credit cards, fullz, dump, cloned, drug, drugs, ketamine, ket, kétamine, ketamin, cocaine, coke, coca, cocain, cocaïne, cannabis, canabis, weed, hash, mdma, ecstasy, ecsta, xtc,md, document, documents, doc, docs, licence, licences, invoice, invoices, identity card, identity cards, identity, card, cards, id card, id cards, french, fr, en, america, american, english, en, spanish, france, français, spain, spania, españa, español, buy, shop, shops, market, markets, marketplace, marketplaces, money, place, community, forum, discussion, chat, international">
     <link rel="stylesheet" href="styles.css">
     <link rel="icon" href="img/logo.png" type="image/x-icon">
 </head>
